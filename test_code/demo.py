2eae447aae8e68b246c0bdfb906b20672b62e3f5
Markay12 <ashinhust.brass@gmail.com>
2020-09-18 11:38:16 -0700

    random demo code to test grades

---
+++ test_code/demo.py
@@ -0,0 +1,22 @@
+# Mark Ashinhust CSE220 Test Run Code for Conditionals and loops
+
+def main():
+    print("Hello")
+
+
+    grade = int(input("Enter your grade: "))
+
+    if grade >= 90:
+        print("A")
+    elif grade >= 80:
+        print("B")
+    elif grade >= 70:
+        print("C")
+    elif grade >= 60:
+        print("D");
+    else:
+        print("E")
+
+
+if __name__ == '__main__':
+    main()
