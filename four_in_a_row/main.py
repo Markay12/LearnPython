8076f9aa2bb985482e70ae79bd384d1b40a5657f
Markay12 <ashinhust.brass@gmail.com>
2020-09-25 11:38:34 -0700

    updated syntax

--- four_in_a_row/main.py
+++ four_in_a_row/main.py
@@ -3,7 +3,7 @@ import numpy as np #import to work with Arrays
 # create matrix to hold pieces from the board
 def playspace():
     gameboard = np.zeros((6, 7)) #create initial matrix of 6 rows and 7 columns
-    return gameboard;
+    return gameboard
 
 playspace = playspace()
 print(playspace)
