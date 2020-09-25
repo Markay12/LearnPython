947cbe00551bf2a9b57e512dcdbac17d083022e2
Markay12 <ashinhust.brass@gmail.com>
2020-09-25 11:38:46 -0700

    further iteration into class methods

--- test_code/classStructure.py
+++ test_code/classStructure.py
@@ -4,13 +4,23 @@ class Box:
     def __init__(self):
         pass
 
+    def doSomething(self):
+        id = 2000
+
 box1 = Box()
 box2 = Box()
 
 
+
 #overshadow the assignment property
 box2.id = 5555
+box2.id2 = 1000
+box1.id2 = 'I am a new variable! Hello!'
 
-print(box1.id)
-print(box2.id)
+
+print(box1.id) #print class property
+print(box2.id) #print the new variable that we just created
+print(box2.id2)
+print(box1.id2)
+
 
